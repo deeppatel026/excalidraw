49307c6a355b868725443092d2ad28164e270b74
deeppatel026 <deepjpatel026@gmail.com>
2025-12-22 21:38:31 -0600

    added zod schema and common module

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,21 +1,34 @@
 import express from "express"
 import jwt from "jsonwebtoken"
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common";
 import { middleware } from "./middleware";
+import { createUserSchema, signinUserSchema, createRoomSchema } from "@repo/common/zodschema"
+
 
 const app = express()
 
 app.post("/signup", (req,res)=> {
 
-    const username = req.body.usermane;
-    const password = req.body.password;
+    //Need to include zod validations here
+    const data = createUserSchema.safeParse(req.body);
+    if(!data.success){
+        res.status(302).json({
+            message : "Incorrect input or miss on the zod validation"
+        })
+    }
 
     //Need a DB call here 
 })
 
 
 app.post("/signin", (req,res)=> {
-    
+      
+    const data = signinUserSchema.safeParse(req.body);
+    if(!data.success){
+        res.status(302).json({
+            message : "Incorrect input or miss on the zod validation"
+        })
+    }
     const userId = 1;
     const token = jwt.sign({
         userId
@@ -30,6 +43,13 @@ app.post("/signin", (req,res)=> {
 
 app.post("/create-room", middleware, (req,res)=> {
     
+    const data = createRoomSchema.safeParse(req.body);
+    if(!data.success){
+        res.status(302).json({
+            message : "Incorrect input or miss on the zod validation"
+        })
+    }
+
     const roomId = 123;
     res.json({
         message: "room created successfully",
---
+++ packages/common/src/zodschema.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod'
+
+export const createUserSchema = z.object({
+    username : z.string().min(3).max(20),
+    password : z.string(),
+    name : z.string()
+})
+
+export const signinUserSchema = z.object({
+    username : z.string().min(3).max(20),
+    password : z.string(),
+    name : z.string()
+})
+
+export const createRoomSchema = z.object({
+    name : z.string().min(3).max(20)
+ })
