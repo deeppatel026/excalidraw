3c5fd61fb4676ec63c5f2b8f331ddb0fbbaabcc3
deeppatel026 <deepjpatel026@gmail.com>
2025-12-24 17:29:18 -0600

    Added db configs

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,30 +3,50 @@ import jwt from "jsonwebtoken"
 import { JWT_SECRET } from "@repo/backend-common";
 import { middleware } from "./middleware";
 import { createUserSchema, signinUserSchema, createRoomSchema } from "@repo/common/zodschema"
-
+import { prismaClient } from "@repo/db"
 
 const app = express()
 
-app.post("/signup", (req,res)=> {
+app.use(express.json())
 
-    //Need to include zod validations here
-    const data = createUserSchema.safeParse(req.body);
-    if(!data.success){
+app.post("/signup", async (req, res) => {
+
+    const parsedData = createUserSchema.safeParse(req.body);
+    if (!parsedData.success) {
         res.status(302).json({
-            message : "Incorrect input or miss on the zod validation"
+            message: "Incorrect input or miss on the zod validation"
+        })
+        return;
+    }
+
+
+
+    try {
+        const userAdd = await prismaClient.user.create({
+            data: {
+                email: parsedData.data.email,
+                password: parsedData.data.password,
+                name: parsedData.data.name
+            }
+        })
+    } catch (e) {
+        res.status(411).json({
+            message: "User already exists or email already registered"
         })
     }
+
+
 
     //Need a DB call here 
 })
 
 
-app.post("/signin", (req,res)=> {
-      
+app.post("/signin", (req, res) => {
+
     const data = signinUserSchema.safeParse(req.body);
-    if(!data.success){
+    if (!data.success) {
         res.status(302).json({
-            message : "Incorrect input or miss on the zod validation"
+            message: "Incorrect input or miss on the zod validation"
         })
     }
     const userId = 1;
@@ -36,27 +56,29 @@ app.post("/signin", (req,res)=> {
 
     res.json({
         message: "You have successfully signed up",
-        token : token
+        token: token
     })
 })
 
 
-app.post("/create-room", middleware, (req,res)=> {
-    
+app.post("/create-room", middleware, (req, res) => {
+
     const data = createRoomSchema.safeParse(req.body);
-    if(!data.success){
+    if (!data.success) {
         res.status(302).json({
-            message : "Incorrect input or miss on the zod validation"
+            message: "Incorrect input or miss on the zod validation"
         })
     }
 
     const roomId = 123;
     res.json({
         message: "room created successfully",
-        roomId : roomId
+        roomId: roomId
     })
 })
 
-app.listen(3001, ()=> {
+app.listen(3001, () => {
     console.log("HTTP server started on 3001")
 })
+
+
